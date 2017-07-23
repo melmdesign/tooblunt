7d14022ca731abfb7b052bdf519dfbf1771090ae
Liam <liamm.designs@gmail.com>
2017-07-23 13:10:24 +0200

    marqueeify

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 			container: $(this).parent(),
 			bumpEdge: function () {}
 		}, options);
-		
+
 		return this.each(function () {
 			var containerWidth, containerHeight, elWidth, elHeight, move, getSizes,
 				$el = $(this);
@@ -66,7 +66,7 @@
 $(document).ready( function() {
 
 	$('.marquee').marqueeify({
-		speed: 300,
+		speed: 200,
 		bumpEdge: function () {
 			var newColor = "hsl(" + Math.floor(Math.random()*360) + ", 100%, 50%)";
 			$('.marquee .logo').css('fill', newColor);
